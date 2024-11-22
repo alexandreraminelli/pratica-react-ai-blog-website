bde57875b7ce05c22359c815427cb1ba70714857
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2024-11-22 10:31:50 -0300

    feat(header): cor de background do header

--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -1,29 +1,22 @@
 /* Header (cabeçalho) do site */
 
+// Estilos CSS
+import styles from "./Header.module.css"
+
 /**
  * Componente do cabeçalho do site.
  * @return O conteúdo JSX do cabeçalho.
  */
 export default function Header(): JSX.Element {
   return (
-    <nav>
-      <ul>
-        <li>
-          <a href="/">Início</a>
-        </li>
-        <li>
-          <a href="/noticias">Notícias</a>
-        </li>
-        <li>
-          <a href="/podcasts">Podcasts</a>
-        </li>
-        <li>
-          <a href="/recursos">Recursos</a>
-        </li>
-        <li>
-          <a href="/contato">Contato</a>
-        </li>
-      </ul>
-    </nav>
+    <>
+      {/* Top Banner */}
+
+      {/* Header Principal */}
+      <header className={`pd-container ${styles.siteHeader}`}>
+        {/* Logo do site */}
+        {/* Navbar */}
+      </header>
+    </>
   )
 }
