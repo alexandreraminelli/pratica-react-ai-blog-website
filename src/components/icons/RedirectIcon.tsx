d5f97d72b011997ad0ad4750409afff4f2aa4ef9
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2024-11-25 12:46:35 -0300

    feat(icons): criação do componente do Ícone de Redirecionamento

---
+++ src/components/icons/RedirectIcon.tsx
@@ -0,0 +1,11 @@
+/**
+ * Componente do ícone de redirecionamento.
+ * @returns O JSX contendo o SVG do ícone.
+ */
+export default function RedirectIcon(): JSX.Element {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+      <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
+    </svg>
+  )
+}
