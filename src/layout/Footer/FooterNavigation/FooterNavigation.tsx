1f2933cc72935bee88f3fb047b7c648bffb01110
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2024-11-27 09:37:30 -0300

    feat(footer): base do FooterNavigation
    Adição de espaçamento no FooterNavigation e criação do componente LinkFooterNav.

--- src/layout/Footer/FooterNavigation/FooterNavigation.tsx
+++ src/layout/Footer/FooterNavigation/FooterNavigation.tsx
@@ -1,6 +1,55 @@
+/* importação de dependências */
+import { Link } from "react-router-dom"
+
+/* módulos CSS */
+import styles from "./FooterNavigation.module.css"
+
 /**
  * Componente da seção de navegação do rodapé.
  */
 export default function FooterNavigation(): JSX.Element {
-  return <nav>Footer Navigation</nav>
+  return (
+    <nav className={styles.FooterNav} aria-label="Navegação do rodapé">
+      {/* Lista de links */}
+      <ul></ul>
+    </nav>
+  )
+}
+
+/** Interface de link */
+interface LinkFooterNavProps {
+  /** Texto exibido na UI */
+  text: string
+  /** Link do endpoint pra página */
+  link: string
+}
+/**
+ *
+ */
+function LinkFooterNav({ text, link }: LinkFooterNavProps): JSX.Element {
+  return (
+    <li>
+      <Link
+        to={link} // endereço do link
+      >
+        {/* Texto do link */}
+        {text}
+      </Link>
+    </li>
+  )
+}
+
+/**
+ * Interface dos props do LinkGroup
+ */
+interface LinkGroupProps {
+  /** Link da página do grupo */
+  linkPagina: LinkFooterNavProps
+}
+
+/**
+ * Componente de grupo de links do FooterNavigation.
+ */
+function LinkGroup(): JSX.Element {
+  return <li>{/* Link da página */}</li>
 }
