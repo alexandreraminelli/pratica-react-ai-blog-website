ec12f8a5f770b2603dcef1e2c7f3ba5f3eeb5da8
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2024-11-27 11:47:35 -0300

    chore(footer): lista dos links movida para um arquivo externo.

--- src/layout/Footer/FooterNavigation/FooterNavigation.tsx
+++ src/layout/Footer/FooterNavigation/FooterNavigation.tsx
@@ -1,6 +1,9 @@
 /* importação de dependências */
 import { Link } from "react-router-dom"
 
+/* importação do JSON com os links do navbar */
+import linksList from "../../../data/footerNavLinks.json"
+
 /* módulos CSS */
 import styles from "./FooterNavigation.module.css"
 
@@ -25,67 +28,6 @@ interface linksListInterface {
  * Componente da seção de navegação do rodapé.
  */
 export default function FooterNavigation(): JSX.Element {
-  /**
-   * Lista de objetos JSON com os links do Footer Navigation.
-   */
-  const linksList: linksListInterface[] = [
-    {
-      title: "Início",
-      link: "/",
-      sections: [
-        { title: "Novidades", sectionId: "novidades" },
-        { title: "Blogs", sectionId: "blogs" },
-        { title: "Recursos", sectionId: "recursos" },
-        { title: "Depoimentos", sectionId: "depoimentos" },
-        { title: "Entre em Contato", sectionId: "contato" },
-        { title: "Newsletter", sectionId: "newsletter" },
-      ],
-    },
-    {
-      title: "Notícias",
-      link: "/noticias",
-      sections: [
-        { title: "Notícias em Alta", sectionId: "noticias-em-alta" },
-        { title: "Vídeos em Destaque", sectionId: "videos-em-destaque" },
-        { title: "Tecnologia", sectionId: "tecnologia" },
-        { title: "Saúde", sectionId: "saude" },
-        { title: "Política", sectionId: "politica" },
-        { title: "Meio Ambiente", sectionId: "meio-ambiente" },
-      ],
-    },
-    {
-      title: "Blogs",
-      link: "/blog",
-      sections: [
-        { title: "Computação Quântica", sectionId: "computacao-quantica" },
-        { title: "Ética em IA", sectionId: "etica-em-ia" },
-        { title: "Exploração Espacial", sectionId: "exploracao-espacial" },
-        { title: "Biotecnologia", sectionId: "biotecnologia" },
-        { title: "Energias Renováveis", sectionId: "energias-renovaveis" },
-        { title: "Biohacking", sectionId: "biohacking" },
-      ],
-    },
-    {
-      title: "Podcasts",
-      link: "/podcasts",
-      sections: [
-        { title: "AI Revolution", sectionId: "ai-revolution" },
-        { title: "TechTalk AI", sectionId: "techtalk-ai" },
-        { title: "AI Conversations", sectionId: "ai-conversations" },
-      ],
-    },
-    {
-      title: "Recursos",
-      link: "/recursos",
-      sections: [
-        { title: "Whitepapers", sectionId: "whitepapers" },
-        { title: "Ebooks", sectionId: "ebooks" },
-        { title: "Relatórios", sectionId: "relatorios" },
-        { title: "Artigos de Pesquisa", sectionId: "artigos-de-pesquisa" },
-      ],
-    },
-  ]
-
   /* Retorno JSX */
   return (
     <nav className={styles.FooterNav} aria-label="Navegação do rodapé">
