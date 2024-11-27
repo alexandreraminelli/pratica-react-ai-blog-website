4c08f423321dfcf5c861c63bb2fa4033137c04a1
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2024-11-27 09:21:33 -0300

    feat(footer): estrutura básica do componente Footer

--- src/layout/Footer/Footer.tsx
+++ src/layout/Footer/Footer.tsx
@@ -1,8 +1,21 @@
 /* Footer (rodapé) do site */
 
+/* importação de componentes */
+import FooterNavigation from "./FooterNavigation/FooterNavigation"
+
+/* módulos CSS */
+import styles from "./Footer.module.css"
+
 /**
- *
+ * Componente do rodapé do site.
  */
 export default function Footer(): JSX.Element {
-  return <p>&copy; Future Tech</p>
+  return (
+    <footer className={`pd-container ${styles.siteFooter}`}>
+      {/* Footer navigation */}
+      <FooterNavigation />
+
+      {/* Footer legal */}
+    </footer>
+  )
 }
---
+++ src/layout/Footer/FooterNavigation/FooterNavigation.tsx
@@ -0,0 +1,6 @@
+/**
+ * Componente da seção de navegação do rodapé.
+ */
+export default function FooterNavigation(): JSX.Element {
+  return <nav>Footer Navigation</nav>
+}
