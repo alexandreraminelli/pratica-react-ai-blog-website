e3e4a1c3ecf1e9e89cb471fdfa669251a86933a1
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2024-11-28 11:44:22 -0300

    feat(cta): adição do componente CallToAction pelo App

---
+++ src/layout/CallToAction/CallToAction.tsx
@@ -0,0 +1,10 @@
+/**
+ * Componente CTA (Call to Action).
+ */
+export default function CallToAction(): JSX.Element {
+  return (
+    <section>
+      <h2>Call to Action</h2>
+    </section>
+  )
+}
