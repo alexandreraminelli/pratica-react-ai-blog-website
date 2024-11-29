4200afe19f84db4e2835703161b2c05415c5d1d6
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2024-11-29 08:11:20 -0300

    chore(cta): componentes do HeadlineCTA movidos para um novo arquivo

--- src/layout/CallToAction/CallToAction.tsx
+++ src/layout/CallToAction/CallToAction.tsx
@@ -1,11 +1,8 @@
 /* importação de componentes */
-import Badge from "../../components/Badge/Badge" // badges
+import HeadlineCTA from "./components/HeadlineCTA" // headline do CTA
 
 /* módulos CSS */
 import styles from "./CallToAction.module.css"
-
-/* importação de imagens */
-import logo from "../../assets/images/logo/logo-favicon.svg"
 
 /**
  * Componente CTA (Call to Action).
@@ -18,52 +15,5 @@ export default function CallToAction(): JSX.Element {
 
       {/* Lista de cards */}
     </section>
-  )
-}
-
-/**
- * Headline do CTA.
- */
-function HeadlineCTA(): JSX.Element {
-  return (
-    <header className={styles.headlineCTA}>
-      {/* Linha do Badge */}
-      <BadgeLine />
-
-      {/* Título e Texto */}
-      <TitleAndTextHeadline />
-    </header>
-  )
-}
-
-/**
- * Linha com a logo e o badge.
- */
-function BadgeLine(): JSX.Element {
-  return (
-    <div className={styles.badgeLine}>
-      {/* Imagem (visível no mobile) */}
-      <img src={logo} alt="Logo da FutureTech" />
-      {/* Badge */}
-      <Badge text="Aprenda, Conecte-se, Inove" />
-    </div>
-  )
-}
-
-/**
- * Título e texto do headline do CTA.
- */
-function TitleAndTextHeadline(): JSX.Element {
-  return (
-    <hgroup>
-      {/* Título */}
-      <h2>Faça parte da revolução tecnológica do futuro</h2>
-
-      {/* Texto mobile */}
-      <p className={styles.mobileText}>Mergulhe no mundo da tecnologia do futuro. Explore nossos recursos abrangentes.</p>
-
-      {/* Texto desktop */}
-      <p className={styles.desktopText}>Mergulhe no mundo da tecnologia do futuro. Explore nossos recursos abrangentes, conecte-se com outros entusiastas da tecnologia e impulsione a inovação no setor. Junte-se a uma comunidade dinâmica de pensadores visionários.</p>
-    </hgroup>
   )
 }
