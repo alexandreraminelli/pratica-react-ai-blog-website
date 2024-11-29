59e847cf739b9f5cf77f1500c26cd2c1cc628f2a
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2024-11-29 09:28:03 -0300

    feat(redirect-card): criação e estilização do componente RedirectCard

---
+++ src/components/Cards/RedirectCard/RedirectCard.tsx
@@ -0,0 +1,50 @@
+/* importação de dependências */
+import { Link } from "react-router-dom"
+/* ícones SVG */
+import RedirectIcon from "../../icons/RedirectIcon"
+/* módulos CSS */
+import styles from "./RedirectCards.module.css"
+
+/**
+ * Interface dos props do componente RedirectCards.
+ */
+interface RedirectCards {
+  /** Título exibido nos cards. */
+  title: string
+  /** Texto dos cards. */
+  text: string
+  /** Link dos cards */
+  link: string
+}
+
+/**
+ * Cards de Redirecionamento. Os cards são compostos por: título, parágrafo e ícone de redirecionamento.
+ */
+export default function RedirectCards({ title, text, link }: RedirectCards): JSX.Element {
+  return (
+    <Link
+      to={link} // endereço
+      className={`no-recolor no-underline ${styles.redirectCard}`} // classes CSS
+    >
+      {/* Headline do Card */}
+      <HeadlineRedirectCard title={title} />
+
+      {/* Texto */}
+      <p>{text}</p>
+    </Link>
+  )
+}
+
+/**
+ * Headline do card de redirecionamento.
+ */
+function HeadlineRedirectCard({ title }: Pick<RedirectCards, "title">): JSX.Element {
+  return (
+    <header className={styles.headlineCard}>
+      {/* Título do card */}
+      <h3>{title}</h3>
+      {/* Ícone */}
+      <RedirectIcon />
+    </header>
+  )
+}
