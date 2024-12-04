9e7d4ca8812ea5d64e2f4164ba50ad08a3cd8a4d
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2024-12-04 17:23:15 -0300

    feat(icons): adição dos ícones abstratos 1, 2 e 3

---
+++ src/components/icons/abstractIcons/AbstractIcon02.tsx
@@ -0,0 +1,11 @@
+/**
+ * Componente do SVG do ícone abstrato 2.
+ */
+export default function AbstractIcon02(): JSX.Element {
+  return (
+    <svg viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M20 28a8 8 0 108-8h-8v8zM20 12a8 8 0 10-8 8h8v-8z" fill="#FFD11A" />
+      <path fillRule="evenodd" clipRule="evenodd" d="M28 4a8 8 0 00-8 8v8h8a8 8 0 100-16zm4 8a4 4 0 11-8 0 4 4 0 018 0zM12 36a8 8 0 008-8v-8h-8a8 8 0 100 16zm4-8a4 4 0 11-8 0 4 4 0 018 0z" fill="#404040" />
+    </svg>
+  )
+}
---
+++ src/components/icons/abstractIcons/AbstractIcon03.tsx
@@ -0,0 +1,11 @@
+/**
+ * Componente do SVG do ícone abstrato 3.
+ */
+export default function AbstractIcon03(): JSX.Element {
+  return (
+    <svg viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M4 4h16c8.837 0 16 7.163 16 16H20C11.163 20 4 12.837 4 4z" fill="#FFD11A" />
+      <path d="M4 20h16c8.837 0 16 7.163 16 16H20c-8.837 0-16-7.163-16-16z" fill="#404040" />
+    </svg>
+  )
+}
