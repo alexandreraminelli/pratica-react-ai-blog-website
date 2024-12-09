25f38a5da1a259256259849088a581c0792eda6a
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2024-12-09 13:39:15 -0300

    feat(features-section): criação do componente `FeaturesSection`

---
+++ src/sections/Home/FeaturesSection/FeaturesSection.tsx
@@ -0,0 +1,13 @@
+/* importação de componentes */
+import HeaderSection from "../../../components/HeaderSection/HeaderSection"
+
+/**
+ * Componente da seção de funcionalidades da página inicial.
+ */
+export default function FeaturesSection(): JSX.Element {
+  return (
+    <section id="funcionalidades">
+      <HeaderSection badge="Desbloqueie o Poder das" title="Funcionalidades da FutureTech" />
+    </section>
+  )
+}
