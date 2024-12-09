b42ca00520abdd14bc7cad6d806def39c5cb6d1c
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2024-12-09 13:32:29 -0300

    feat(header-section): opção de adicionar um RedirectButton

--- src/components/HeaderSection/HeaderSection.tsx
+++ src/components/HeaderSection/HeaderSection.tsx
@@ -1,5 +1,6 @@
 /* importação de componentes */
 import Badge from "../Badge/Badge"
+import RedirectButton, { RedirectButtonProps } from "../Buttons/RedirectButton/RedirectButton"
 
 /* módulos CSS*/
 import styles from "./HeaderSection.module.css"
@@ -12,18 +13,24 @@ interface HeaderSectionProps {
   badge: string
   /** Título da seção. */
   title: string
+  /** Props pro botão de redirecionamento.
+   *  Deve ser passado como um objeto JSON. (opcional) */
+  button?: Omit<RedirectButtonProps, "darkBackground">
 }
 
 /**
  * Componente do cabeçalho de seções.
  */
-export default function HeaderSection({ badge, title }: HeaderSectionProps): JSX.Element {
+export default function HeaderSection({ badge, title, button }: HeaderSectionProps): JSX.Element {
   return (
     <header className={`pd-container ${styles.headerSection}`}>
+      {/* Badge e título */}
       <hgroup>
         <Badge text={badge} />
         <h2>{title}</h2>
       </hgroup>
+      {/* Botão de redirecionamento */}
+      {button && <RedirectButton darkBackground className={styles.button} {...button} />}
     </header>
   )
 }
