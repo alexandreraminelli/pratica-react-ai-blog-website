2c78a7fa10f5dbaa0c3c7be5f703966ece4e9f62
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2024-12-14 18:39:56 -0300

    feat(icons): adição dos ícones de like, compartilhamento e comentários

---
+++ src/components/icons/PostIcons/CommentIcon.tsx
@@ -0,0 +1,8 @@
+/** Componente SVG do ícone de comentários. */
+export default function CommentIcon(): JSX.Element {
+  return (
+    <svg viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M7.072 16.49a6.667 6.667 0 10-2.621-2.296m2.62 2.297l-3.737.676 1.116-2.972m2.622 2.296l.005-.001M4.45 14.195l.001-.003" stroke="currentColor" strokeWidth={1.5} strokeLinecap="round" strokeLinejoin="round" />
+    </svg>
+  )
+}
---
+++ src/components/icons/PostIcons/ShareIcon.tsx
@@ -0,0 +1,8 @@
+/** Componente SVG do ícone de compartilhar. */
+export default function ShareIcon(): JSX.Element {
+  return (
+    <svg viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M8.366 12.134l-4.89-2.222a.833.833 0 01.046-1.536L15.75 3.673a.833.833 0 011.077 1.077l-4.703 12.228a.833.833 0 01-1.536.045l-2.222-4.889zm0 0l3.654-3.653" stroke="currentColor" strokeWidth={1.5} strokeLinecap="round" strokeLinejoin="round" />
+    </svg>
+  )
+}
