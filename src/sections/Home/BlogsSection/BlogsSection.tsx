68a74c2fbc45b5b0ce7a77c6204061b165ba1cf0
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2024-12-14 18:07:56 -0300

    chore(blogs-section): adição no JSON dos conteúdos dos posts

--- src/sections/Home/BlogsSection/BlogsSection.tsx
+++ src/sections/Home/BlogsSection/BlogsSection.tsx
@@ -8,7 +8,8 @@ interface BlogsSectionContent {
   header: HeaderSectionProps
   /** Lista de tabs com as categorias dos blogs. */
   tabs: PostsSectionProps["tabsList"]
-  /** Lista  */
+  /** Lista de posts que serão exibidos na seção. */
+  postList: PostsSectionProps["postList"]
 }
 
 /** Componente da seção de páginas. */
@@ -21,6 +22,44 @@ export default function BlogsSection(): JSX.Element {
       button: { text: "Ver todos os blogs", link: "/blog" },
     },
     tabs: [{ name: "Todos", active: true }, { name: "Computação Quântica" }, { name: "Ética da IA" }, { name: "Exploração Espacial" }, { name: "Biotecnologia" }, { name: "Energia Renovável" }],
+    postList: [
+      {
+        profile: { name: "John Techson" },
+        category: "Computação Quântica",
+        title: "O Salto Quântico na Computação",
+        text: "Explore a revolução na computação quântica, suas aplicações e seu potencial impacto em vários setores.",
+        date: "15 de outubro de 2023",
+        info: {
+          likes: 24.5,
+          comments: 50,
+          shares: 20,
+        },
+      },
+      {
+        profile: { name: "John Techson" },
+        category: "Computação Quântica",
+        title: "O Salto Quântico na Computação",
+        text: "Explore a revolução na computação quântica, suas aplicações e seu potencial impacto em vários setores.",
+        date: "15 de outubro de 2023",
+        info: {
+          likes: 24.5,
+          comments: 50,
+          shares: 20,
+        },
+      },
+      {
+        profile: { name: "John Techson" },
+        category: "Computação Quântica",
+        title: "O Salto Quântico na Computação",
+        text: "Explore a revolução na computação quântica, suas aplicações e seu potencial impacto em vários setores.",
+        date: "15 de outubro de 2023",
+        info: {
+          likes: 24.5,
+          comments: 50,
+          shares: 20,
+        },
+      },
+    ],
   }
 
   /* Retorno JSX */
@@ -29,7 +68,7 @@ export default function BlogsSection(): JSX.Element {
       {/* Cabeçalho */}
       <HeaderSection badge={content.header.badge} title={content.header.title} button={content.header.button} />
       {/* Corpo */}
-      <PostsSection tabsList={content.tabs} />
+      <PostsSection tabsList={content.tabs} postList={content.postList} />
     </section>
   )
 }
